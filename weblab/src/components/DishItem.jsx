9ac8e575557769bb6c440b6b7ea81ce09d348045
kevin <kevin.bernard1901@gmail.com>
2024-04-08 09:38:30 +0200

    Profile Bar img

--- weblab/src/components/DishItem.jsx
+++ weblab/src/components/DishItem.jsx
@@ -16,6 +16,7 @@ function DishItem({ dishId }) {
       <h3>{dish[0].name}</h3>
       <p>{dish[0].description}</p>
       <p>{dish[0].selling_price}</p>
+      <img {dish[0]?.image}/>
     </div>
   );
 }
