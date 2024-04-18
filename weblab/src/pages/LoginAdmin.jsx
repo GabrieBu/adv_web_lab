2b73db6e8b84d7d0f27a1dd5b33a69fce7f75fd7
2024Huakang <163312801+2024Huakang@users.noreply.github.com>
2024-04-19 01:06:35 +0200

    Add files via upload

--- weblab/src/pages/LoginAdmin.jsx
+++ weblab/src/pages/LoginAdmin.jsx
@@ -14,31 +14,50 @@ function LoginAdmin() {
   }
 
   return (
-    <div>
-      <h2>Staff Login</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            id="email"
-            {...register("email", {
-              required: "This field is required",
-            })}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            {...register("password", {
-              required: "This field is required",
-            })}
-          />
-        </div>
-        <button type="submit">{!isLoading ? "Login" : <Loader />}</button>
-      </form>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh', // Full height of the viewport
+      backgroundColor: '#f0f0f0'
+    }}>
+      <div style={{
+        width: '300px',
+        padding: '50px',
+        backgroundColor: '#fff',
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+        borderRadius: '10px',
+        textAlign: 'center'
+      }}>
+        <h2 style={{ color: '#333' }}>Staff Login</h2>
+        <form onSubmit={handleSubmit(onSubmit)} style={{ display: 'flex', flexDirection: 'column' }}>
+          <div style={{ marginBottom: '20px' }}>
+            <label htmlFor="email" style={{ marginBottom: '5px', display: 'block', fontWeight: 'bold' }}>Email</label>
+            <input
+              type="text"
+              id="email"
+              style={{ padding: '10px', borderRadius: '5px', border: '1px solid #ccc', width: '100%' }}
+              {...register("email", {
+                required: "This field is required",
+              })}
+            />
+          </div>
+          <div style={{ marginBottom: '20px' }}>
+            <label htmlFor="password" style={{ marginBottom: '5px', display: 'block', fontWeight: 'bold' }}>Password</label>
+            <input
+              type="password"
+              id="password"
+              style={{ padding: '10px', borderRadius: '5px', border: '1px solid #ccc', width: '100%' }}
+              {...register("password", {
+                required: "This field is required",
+              })}
+            />
+          </div>
+          <button type="submit" style={{ backgroundColor: '#007BFF', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
+            {!isLoading ? "Login" : <Loader />}
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
