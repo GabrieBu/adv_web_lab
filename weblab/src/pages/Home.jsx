d3a5579d7248adc39fa955bff89e40c283da5923
kevin <kevin.bernard1901@gmail.com>
2024-04-21 22:31:39 +0200

    Search filter

--- weblab/src/pages/Home.jsx
+++ weblab/src/pages/Home.jsx
@@ -4,6 +4,9 @@ import ProfileBar from "../components/ProfileBar";
 import DishItem from "../components/DishItem";
 import { supabase } from "../services/db/supabase";
 import useTitle from "../hooks/useTitle";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
   useTitle("Home - Menu");
@@ -16,18 +19,58 @@ function Home() {
     },
   });
 
+  const [searchTerm, setSearchTerm] = useState("");
+  const [showSearchBar, setShowSearchBar] = useState(false);
+
   if (isLoadingDishes) return <Loader />;
 
   return (
     <div>
       <ProfileBar />
-      <div>
-        {dishes.map((item, i) => (
-          <DishItem key={i} dishId={item.id_food_drink} />
-        ))}
+      <div style={{ padding: "10px" }}>
+        <nav className="navbar navbar-light bg-light justify-content-end">
+          {showSearchBar && (
+            <form className="form-inline">
+              <input 
+                className="form-control mr-sm-2" 
+                type="search" 
+                placeholder="Search for a dish" 
+                aria-label="Search" 
+                onChange={(event) => {
+                  setSearchTerm(event.target.value);
+              }} />
+            </form>
+          )}
+          <button 
+            className="navbar-toggler"
+            onClick={() => setShowSearchBar(!showSearchBar)}>
+            <FontAwesomeIcon icon={faSearch} size="2x" />
+          </button>
+        </nav>
+        <p></p>
+        <p></p>
+        <div>
+          {dishes
+            .filter((dish) => {
+              if (searchTerm === "") {
+                return dish;
+              }
+              else if (dish.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                return dish;
+              }
+            })
+            .map((dish) => (
+              <DishItem key={dish.id_food_drink} dishId={dish.id_food_drink} />
+            ))
+          }
+        </div>
       </div>
     </div>
   );
 }
+
+// {dishes.map((item, i) => (
+//   <DishItem key={i} dishId={item.id_food_drink} />
+
 
 export default Home;
