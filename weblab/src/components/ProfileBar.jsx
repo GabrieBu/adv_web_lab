0b1934806c72202ab0923cd166aa9bdfcd1b41df
kevin <kevin.bernard1901@gmail.com>
2024-04-27 19:03:27 +0200

    Changement of the ProfileBar when we go to Order page

--- weblab/src/components/ProfileBar.jsx
+++ weblab/src/components/ProfileBar.jsx
@@ -2,7 +2,27 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faList, faUser } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
-function ProfileBar() {
+function ProfileBar({ active_page }) {
+  let backgroundColor_home = "green";
+  let color_home = "white";
+  let backgroundColor_order = "white";
+  let color_order = "green";
+  let backgroundColor_profile = "white";
+  let color_profile = "green";
+
+  if (active_page === "order") {
+    backgroundColor_home = "white";
+    color_home = "green";
+    backgroundColor_order = "green";
+    color_order = "white";
+  } else if (active_page === "profile") {
+    backgroundColor_home = "white";
+    color_home = "green";
+    backgroundColor_profile = "green";
+    color_profile = "white";
+  }
+
+
   return (
     <ul className="nav nav-pills nav-fill fixed-bottom">
       <li className="nav-item">
@@ -10,7 +30,7 @@ function ProfileBar() {
           to="/home"
           className="nav-link active"
           aria-current="page"
-          style={{ borderRadius: "0px", backgroundColor: "green"}}
+          style={{ borderRadius: "0px", backgroundColor: backgroundColor_home, color: color_home}}
         >
           <div className="d-flex flex-column align-items-center">
             <FontAwesomeIcon icon={faBars} />
@@ -22,7 +42,7 @@ function ProfileBar() {
         <Link
           to="/order"
           className="nav-link"
-          style={{ borderRadius: "0px", backgroundColor: "white", color: "green" }}
+          style={{ borderRadius: "0px", backgroundColor: backgroundColor_order, color: color_order }}
         >
           <div className="d-flex flex-column align-items-center">
             <FontAwesomeIcon icon={faList} />
@@ -34,7 +54,7 @@ function ProfileBar() {
         <Link
           to="/profile"
           className="nav-link"
-          style={{ borderRadius: "0px", backgroundColor: "white", color: "green"}}
+          style={{ borderRadius: "0px", backgroundColor: backgroundColor_profile, color: color_profile}}
         >
           <div className="d-flex flex-column align-items-center">
             <FontAwesomeIcon icon={faUser} />
